277c27d78d6dbd7e72298edd22b93c26f1378103
Steve Godin <steve.god@gmail.com>
2016-11-25 13:53:22 -0800

    Install jQuery and make first test AJAX requests

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
+
+const BASE_URL = 'http://localhost:3001';
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      questions: []
+    }
+  }
+
+  getQuestions() {
+    $.ajax({
+      url: `${BASE_URL}/api/v1/questions`,
+      success: function (questions) {
+        // once we receive the questions from our server,
+        // we store in the state
+        this.setState({questions: questions})
+      // this function is called asynchronously
+      // we must bind(this) to be able to use this.setState
+      }.bind(this)
+    })
+  }
+
+  componentDidMount () {
+    // when the component is loaded on the page, we
+    // will make an ajax request to fetch the questions
+    this.getQuestions()
+  }
+
   render() {
     return (
       <div className="App">
-
+        {
+          // JSON.stringify converts a javascript object
+          // into a JSON string
+          JSON.stringify(this.state.questions)
+        }
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import $ from 'jquery'
 // Behind the scenes, the Webpack asset manager
 // will load css files specified in import statements
 import './index.css';
@@ -9,3 +10,8 @@ ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+// If you want access to variable globally in the console, you
+// can set it as a property of the global window object
+// 👇 only do this for testing
+// window.$ = $;
