00993a337b22b4ec4149060bba5220f1c19246ca
Steve Godin <steve.god@gmail.com>
2016-11-25 14:42:54 -0800

    Implement clickable question list

--- src/App.js
+++ src/App.js
@@ -26,6 +26,10 @@ class App extends Component {
     })
   }
 
+  expandQuestion (id) {
+    console.log('Question id:', id)
+  }
+
   componentDidMount () {
     // when the component is loaded on the page, we
     // will make an ajax request to fetch the questions
@@ -35,7 +39,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <QuestionList questions={this.state.questions} />
+        <QuestionList
+          questions={this.state.questions}
+          onClick={this.expandQuestion} />
       </div>
     );
   }
--- src/QuestionList.js
+++ src/QuestionList.js
@@ -1,13 +1,35 @@
 import React from 'react';
 
 function QuestionList (props) {
+
+  const forwardQuestion = function (id) {
+    // function that returns a function
+    // when forwardQuestion is called, the id argument
+    // will be accessible to the function below
+    return function (event) {
+      props.onClick(id)
+    }
+  }
+
+  // var questionFunc = forwardQuestion(10)
+  // ☝️ will be a function
+  // it will be
+  // function (event) {
+  //   props.onClick(id)
+  // }
+  // the id variable will be 10 as declared above
+
   return (
     <ul className='QuestionList'>
       {
         props.questions.map(
           function (question, index) {
             return (
-              <li key={question.id}>{question.title}</li>
+              <li
+                onClick={forwardQuestion(question.id)}
+                key={question.id}>
+                {question.title}
+              </li>
               )
           }
         )
@@ -22,7 +44,10 @@ function QuestionList (props) {
 // This object will contain keys named after your props. Its values
 // will be the default values.
 QuestionList.defaultProps = {
-  questions: []
+  questions: [],
+  // to prevent a crash if the QuestionList component is
+  // called without an onClick prop with set a default empty function to it
+  onClick: function () {}
 }
 
 // We can export any variable
