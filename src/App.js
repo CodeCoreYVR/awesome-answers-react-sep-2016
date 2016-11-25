ded90af6b2728c86dfd1bdf6966fea1aee95f6c3
Steve Godin <steve.god@gmail.com>
2016-11-25 15:05:11 -0800

    Add question detail component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import QuestionList from './QuestionList';
+import QuestionDetail from './QuestionDetail'
 import $ from 'jquery';
 
 const BASE_URL = 'http://localhost:3001';
@@ -9,8 +10,27 @@ class App extends Component {
     super(props)
 
     this.state = {
-      questions: []
+      questions: [],
+      question: undefined
     }
+
+    // expandQuestion will be used asynchronously as a callback
+    // when a question is clicked
+    // we must garantee that it still has this. bind it!
+    this.expandQuestion = this.expandQuestion.bind(this);
+  }
+
+  getQuestion(id) {
+    $.ajax({
+      url: `${BASE_URL}/api/v1/questions/${id}`,
+      success: function (question) {
+        // once we receive the questions from our server,
+        // we store in the state
+        this.setState({question: question})
+      // this function is called asynchronously
+      // we must bind(this) to be able to use this.setState
+      }.bind(this)
+    })
   }
 
   getQuestions() {
@@ -27,7 +47,9 @@ class App extends Component {
   }
 
   expandQuestion (id) {
-    console.log('Question id:', id)
+    // expandQuestion is called when a question from the
+    // QuestionList component is clicked
+    this.getQuestion(id)
   }
 
   componentDidMount () {
@@ -37,11 +59,21 @@ class App extends Component {
   }
 
   render() {
+    var content;
+
+    // If we have the data of a question stored in the state, let's
+    // render it. Otherwise, render the list of questions
+    if (this.state.question) {
+      content = <QuestionDetail question={this.state.question} />
+    } else {
+      content = <QuestionList
+        questions={this.state.questions}
+        onClick={this.expandQuestion} />
+    }
+
     return (
       <div className="App">
-        <QuestionList
-          questions={this.state.questions}
-          onClick={this.expandQuestion} />
+        { content }
       </div>
     );
   }
